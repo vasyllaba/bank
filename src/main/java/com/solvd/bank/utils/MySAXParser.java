f2e50376aca919d4839a0343e14fbe2593699c8e
Vasil Laba <vasillaba1@gmail.com>
2023-01-24 18:28:15 +0200

    Refactor Sax parser

--- src/main/java/com/solvd/bank/utils/MySAXParser.java
+++ src/main/java/com/solvd/bank/utils/MySAXParser.java
@@ -2,18 +2,23 @@ package com.solvd.bank.utils;
 
 import com.solvd.bank.enums.Role;
 import com.solvd.bank.models.Client;
+import org.apache.log4j.Logger;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+
 public class MySAXParser extends DefaultHandler {
-    final String fileName = "src/main/java/com/solvd/bank/utils/Clients.xml";
+    final String FILE_PATH = "src/main/java/com/solvd/bank/utils/Clients.xml";
+    private static final Logger LOGGER = Logger.getLogger(MySAXParser.class);
 
     private String currentTag = "";
     private Client client;
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        LOGGER.info("call startElement method");
+
         switch (qName) {
             case "clients":
                 break;
@@ -42,6 +47,8 @@ public class MySAXParser extends DefaultHandler {
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
+        LOGGER.info("call characters method");
+
         switch (currentTag) {
             case "passport_id":
                 client.setPassportId(Long.valueOf(new String(ch, start, length)));
@@ -62,10 +69,14 @@ public class MySAXParser extends DefaultHandler {
 
     @Override
     public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+        LOGGER.info("call endElement method");
+
         currentTag = "";
     }
 
     public Client getClient(){
+        LOGGER.info("call getClient method");
+
         return client;
     }
 }
